1db32b11223d13cfd8102d73db55dbd0c051c825
picot244 <picot244@etu.unicaen.fr>
2025-04-04 16:12:13 +0200

    Dessin

--- gribouille/src/main/java/module-info.java
+++ gribouille/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module iut.gon.gribouille {
     requires javafx.controls;
     requires javafx.fxml;
+	requires javafx.graphics;
 
     opens iut.gon.gribouille to javafx.fxml;
     exports iut.gon.gribouille;
