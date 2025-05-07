7692732d82ba5609bfaf05d2b2314dbaf0202fa7
picot244 <solal.picot@etu.unicaen.fr>
2025-05-07 10:58:14 +0200

    Gribouille tp3 complet

--- gribouille/src/main/java/iut/gon/gribouille/App.java
+++ gribouille/src/main/java/iut/gon/gribouille/App.java
@@ -5,10 +5,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.io.IOException;
+
+import iut.gon.modele.Dessin;
 
 /**
  * JavaFX App
@@ -16,39 +18,30 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
-    private static double prevX, prevY;
-    private static Canvas dessin;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("CadreGribouille"), 600, 400);
-        dessin = (Canvas) scene.lookup("Canvas");
+    	Dessin dessin = new Dessin();
+    	Controleur c = new Controleur(dessin);
+        scene = new Scene(loadFXML("CadreGribouille", c), 600, 400);
+        stage.titleProperty().bind(dessin.nomDuFichierProperty());
         stage.setScene(scene);
         stage.show();
         stage.setOnCloseRequest(event -> {
         	if(!Dialogues.confirmation())
         		event.consume();
         });
-        dessin.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> {
-        	prevX = event.getX();
-        	prevY = event.getY();
-        });
-        dessin.addEventHandler(MouseEvent.MOUSE_DRAGGED, event -> {
-        	dessin.getGraphicsContext2D().strokeLine(prevX, prevY, event.getX(), event.getY());
-        	prevX = event.getX();
-        	prevY = event.getY();
-        });
     }
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
+    static void setRoot(String fxml, Controleur c) throws IOException {
+        scene.setRoot(loadFXML(fxml, c));
     }
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+    private static Parent loadFXML(String fxml, Controleur c) throws IOException {
+    	FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+    	fxmlLoader.setController(c);
         return fxmlLoader.load();
     }
-    
     
     public static void main(String[] args) {
         launch();
---
+++ gribouille/src/main/java/iut/gon/modele/package-info.java
@@ -0,0 +1,4 @@
+/**
+ Le modèle pour mémoriser les Dessins avec leurs Figures et leurs Points
+ */
+package iut.gon.modele;
