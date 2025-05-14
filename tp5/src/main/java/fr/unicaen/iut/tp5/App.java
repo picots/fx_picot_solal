4653689b2097259e382a1d8dcb80b04f3d970d00
picot244 <solal.picot@etu.unicaen.fr>
2025-05-14 14:30:47 +0200

    La grille

---
+++ tp5/src/main/java/fr/unicaen/iut/tp5/App.java
@@ -0,0 +1,25 @@
+package fr.unicaen.iut.tp5;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+	  @Override
+	  public void start(Stage stage) throws IOException {
+	    FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("tp5.fxml"));
+	    Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+	    
+	    stage.setTitle("Démineur");
+	    stage.setScene(scene);
+	    stage.show();
+	  }
+
+	  public static void main(String[] args) {
+	    launch();
+	  }
+}
+
--- tp5/src/main/java/fr/unicaen/iut/tp5/ControleurDemineur.java
+++ tp5/src/main/java/fr/unicaen/iut/tp5/ControleurDemineur.java
@@ -8,11 +8,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.RadioMenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 public class ControleurDemineur implements Initializable{
 	
-	ModeleDemineur modele;
+	ModeleDemineur modele = new ModeleDemineur(0,0,0);
 	@FXML ToggleGroup difficulte;
 	@FXML RadioMenuItem facile;
 	@FXML RadioMenuItem moyen;
@@ -25,13 +27,25 @@ public class ControleurDemineur implements Initializable{
 	public void initialize(URL location, ResourceBundle resources) {
 		nbInconnues.textProperty().bind(modele.nbInconnuesProperty().asString());
 		nbMarques.textProperty().bind(modele.nbMarquesProperty().asString());
-		difficulte.selectedToggleProperty().addListener(obs -> {
-			if(facile.isSelected())
-				initGrille(facile.getUserData().toString());
-			if(moyen.isSelected())
-				initGrille(moyen.getUserData().toString());
-			if(difficile.isSelected())
-				initGrille(difficile.getUserData().toString());
-		});
+		difficulte.selectedToggleProperty().addListener((obs, oldToogle, newToogle) -> initGrille(newToogle.getUserData().toString()));
+	}
+	
+	public void initGrille(String userData) {
+		gridPane.getColumnConstraints().clear();
+		gridPane.getRowConstraints().clear();
+		
+		int[] tab = ModeleDemineur.parseUserData(userData);
+		int nbCol = tab[0];
+		int nbLig = tab[1];
+		int nbMines = tab[2];
+		
+		modele = new ModeleDemineur(nbCol, nbLig, nbMines);
+		
+		for(int c = 0; c < nbCol; c++)
+			gridPane.getColumnConstraints().add(new ColumnConstraints(32,32,32));
+		
+		for(int l = 0; l < nbLig; l++)
+			gridPane.getRowConstraints().add(new RowConstraints(32,32,32));
+		
 	}
 }
